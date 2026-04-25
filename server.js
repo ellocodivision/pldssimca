2f1cf2347536dc3c7cdf4145e6768355fcb6d887
ellocodivision <thedivisionmartinillo1983@gmail.com>
2026-04-24 20:06:07 -0500

    Improve Viceroy PH, view, and plano handling

--- server.js
+++ server.js
@@ -5126,7 +5126,7 @@ function parseViceroyInventoryNewLayout(sheet, headerRowNumber) {
       development: '',
       unidad: unit,
       level,
-      groupCode: '',
+      groupCode: groupRaw,
       planLink,
       recamaras,
       edificio: '',
